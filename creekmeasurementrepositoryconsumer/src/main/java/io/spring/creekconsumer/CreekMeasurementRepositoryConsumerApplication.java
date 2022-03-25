5eab4e8d888909433cf9c46467229dcbb2d1c50d
Glenn Renfro <grenfro@vmware.com>
2022-03-25 16:39:14 -0400

    Added CreekMeasurementRepositoryConsumer as new consumer

---
+++ creekmeasurementrepositoryconsumer/src/main/java/io/spring/creekconsumer/CreekMeasurementRepositoryConsumerApplication.java
@@ -0,0 +1,25 @@
+package io.spring.creekconsumer;
+
+import java.util.function.Consumer;
+
+import io.spring.creekfunctions.CreekDataRepository;
+import io.spring.creekfunctions.CreekMeasurementRepository;
+import javax.sql.DataSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+
+@SpringBootApplication
+public class CreekMeasurementRepositoryConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreekMeasurementRepositoryConsumerApplication.class, args);
+	}
+
+	@Bean
+	public Consumer<Message<String>> creekMeasurementRepository(DataSource dataSource) {
+		return new CreekMeasurementRepository(dataSource);
+	}
+}
---
+++ creekmeasurementrepositoryconsumer/src/test/java/io/spring/creekconsumer/CreekMeasurementRepositoryConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.creekconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreekMeasurementRepositoryConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
